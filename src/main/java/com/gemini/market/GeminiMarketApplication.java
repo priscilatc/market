712740289767e62eb15b3d121d53c74bd964eb41
priscilatc <priscila.tinoco.tc@gmail.com>
2022-10-20 00:14:09 -0600

    Creación de proyecto

---
+++ src/main/java/com/gemini/market/GeminiMarketApplication.java
@@ -0,0 +1,13 @@
+package com.gemini.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminiMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeminiMarketApplication.class, args);
+	}
+
+}
